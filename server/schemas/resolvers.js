201f95cfa3b205e44bb45f2d52a9dc52ae948f2c
[Andrew Core] <[adcore89@gmail.com]>
2021-07-19 19:22:01 -0400

    .gitignores created, server schemas set up, connection in server js setup

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+const resolvers = {
+    Query: {
+      helloWorld: () => {
+        return 'Hello world!';
+      }
+    }
+  };
+  
+  module.exports = resolvers;
+  That's all we need to get this sample
